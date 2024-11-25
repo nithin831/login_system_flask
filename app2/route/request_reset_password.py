e778353349c62cb0b971844144f040e750fdcb84
nithin831 <nithinkumar83161@gmail.com>
2024-11-25 09:30:48 +0530

    optimized code with reset password1

---
+++ app2/route/request_reset_password.py
@@ -0,0 +1,36 @@
+from flask import jsonify
+from utils.jwt_utils import generate_verification_token
+from utils.email_utils import send_reset_password_email
+from config import Config
+from user import *
+from validate import *
+
+# Request password reset
+def request_password_reset(data):
+    email = data.get('email')
+    if not email:
+        return jsonify({"error": "Email is required"}), 400
+    try:
+        validate_user_email(email)
+        user = check_user_exist(data)
+        if user:
+            is_active, is_blacklisted = user
+            if is_blacklisted:
+                return jsonify({"error": "This email address is blacklisted, Please contact support."}), 400
+            if not is_active:
+                return jsonify({"error": "User not found"}), 404
+        else:
+            return jsonify({"error": "User not found"}), 404
+
+        # Generate password reset token
+        reset_token = generate_verification_token(email)
+
+        # Send the password reset email
+        reset_link = f"{Config.FRONTEND_URL}/reset-password?token={reset_token}"
+        send_reset_password_email(email, reset_link)
+
+        return jsonify({"message": "Password reset email has been sent. Please check your inbox."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+        
+
